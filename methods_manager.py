3e4fb3307209a2e0bfcde0d359292069d4bb2227
MaferMazu <13-10787@usb.ve>
2021-04-17 08:19:07 -0400

    documentation

--- methods_manager.py
+++ methods_manager.py
@@ -1,12 +1,19 @@
 
 class MethodsManager:
-    """ Methods Manager Class """
+    """My Methods Manager
+    """
     def __init__(self):
         self.heap = {}
 
     def insert(self, elems):
+        """Insert for main
+        Args:
+            elems (list): Tokens form user input
+        """
         if elems[1][0].isupper():
             name = elems[1]
+
+            # I have super?
             if ":" in elems[2:]:
                 if self.have(elems[3]):
                     super_class = elems[3]
@@ -20,14 +27,13 @@ class MethodsManager:
                     print(f"Se creo {name} con sus métodos {str_methods}\n")
                 else:
                     print(f"Error: {elems[3]} no es una clase declarada\n")
-                
+            
+            # I dont have super
             else:
                 super_class = None
                 if len(elems)>1:
-                    print("here")
                     methods = elems[2:]
                 else:
-                    print("vacio")
                     methods=[]
 
                 self.insert_simple(name, super_class, *methods)
@@ -39,10 +45,21 @@ class MethodsManager:
 
 
     def insert_simple(self, name, super_class, *kwargs):
+        """Format my input
+        Args:
+            name (string): Name of my Class
+            super_class (string): Name of my Super Class
+        """
         elem = {"super":super_class, "methods":[*kwargs]}
         self.heap[name]=elem
 
     def have(self, name):
+        """To know if i have a class with this name
+        Args:
+            name (string): Name of my Class
+        Returns:
+            bool: True if name is in my heap else False
+        """
         try:
             self.heap[name]
             return True
@@ -50,20 +67,32 @@ class MethodsManager:
             return False
 
     def search_methods(self, name):
+        """Description for main
+        Args:
+            name (string): Name of my Class
+        Returns:
+            string: String of all of methods for my Class
+        """
         if self.have(name):
             base = self.heap[name]
             ancestors=[name]
+            
+            # Search for ancestors
             while base["super"]!=None:
                 ancestors.append(base["super"])
                 base=self.heap[base["super"]]
 
+            # Older first
             ancestors=ancestors[::-1]
             methods_with_ancestor={}
+
+            # For ancestor insert method
             for ancestor in ancestors:
                 methods=self.heap[ancestor]["methods"]
                 for method in methods:
                     methods_with_ancestor[method]=ancestor
 
+            # Pretty print
             response = ""
             for method in methods_with_ancestor:
                 response = response + f"{method} -> {methods_with_ancestor[method]} :: {method}\n"
